76f627a92f0d41d854958d45fe50378ec2c136c6
Lucas <ns_lucas@hotmail.com>
2025-09-15 15:00:41 -0300

    Criando metodo de cadastro de produtos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 # IMPORTANDO BANCO DE DADOS SQLALCHEMY- SALVA OS DADOS EM UM ARQUIVO TXT 
 # USADO EM APLICACOES MAIS SIMPLES OU EM MODO DEV
@@ -22,9 +22,21 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-    
+
 with app.app_context():
     db.create_all()
+
+
+# DEFININDO ROTA DE CADASTRO DE PRODUTOS
+@app.route('/api/products/add', methods=['POST']) # Rota aceita somente metodo post
+
+def add_product():
+    data = request.json
+    product = Product(name=data['name'], price=data['price'], description=data.get('description', ''))
+    db.session.add(product)
+    db.session.commit()
+
+    return 'Produto cadastrado com sucesso !'
 
 # DEFININDO ROTA RAIZ E FUNCAO QUE SERA EXECUTADA AO REQUISITAR
 @app.route('/')
