131e584bef7b765a36ec9a2a456aa7e6782a2572
Lucas <ns_lucas@hotmail.com>
2025-09-15 14:32:13 -0300

    Criando tabela produto

--- app.py
+++ app.py
@@ -8,9 +8,23 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__) 
 
 # DEFININDO CAMINHO DO BANCO DE DADOS
-app.config['SQLAlchemy_DATABASE_URL'] = 'sqlite:///ecommerce.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+
+# MODELANDO BANCO DE DADOS
+
+# Produto (id, name, price, description)
+class Product(db.Model):
+    # Definindo colunas da tabela produto
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+    
+with app.app_context():
+    db.create_all()
 
 # DEFININDO ROTA RAIZ E FUNCAO QUE SERA EXECUTADA AO REQUISITAR
 @app.route('/')
